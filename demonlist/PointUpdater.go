a4b7911e5cf13650c37ba72ddfd3b0df98ab17c7
Simon <simon.giesse@gmx.de>
2023-08-21 15:37:17 +0200

    Typo

--- demonlist/PointUpdater.go
+++ demonlist/PointUpdater.go
@@ -24,7 +24,7 @@ func RegisterUpdatePoints(app core.App) {
 				return math.Sqrt(value), nil
 			},
 		}
-		formular, err := govaluate.NewEvaluableExpressionWithFunctions(e.Record.GetString("formular"), functions)
+		formular, err := govaluate.NewEvaluableExpressionWithFunctions(e.Record.GetString("formula"), functions)
 		if err != nil {
 			return err
 		}
